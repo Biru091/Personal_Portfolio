bf04d9dd57924ce4c08a5357bdf135ed44280b0b
Birendra Singh Bohara <birendrabohara091@gmail.com>
2026-09-01 19:40:12 +0545

    updated

--- src/app/components/Hero-section/Hero-content/HeroContent.tsx
+++ src/app/components/Hero-section/Hero-content/HeroContent.tsx
@@ -76,13 +76,13 @@ export default function HeroContent() {
         </div>
 
        
-        <div className="absolute bottom-8 right-8 flex items-center gap-3 text-xs uppercase tracking-[0.2em] text-blue-700 animate-pulse">
+        <div className="absolute bottom-14 right-8 flex items-center gap-3 text-xs uppercase tracking-[0.2em] text-blue-700 animate-pulse">
           Scroll
           <ArrowDown size={16} />
         </div>
 
         
-        <div className="text2 absolute bottom-16 left-7  w-2/7 items-center gap-3 text-wrap uppercase tracking-[0.2em] text-black md:flex md:text-2xl">
+        <div className="text2 absolute bottom-20 left-7  w-2/7 items-center gap-3 text-wrap uppercase tracking-[0.2em] text-black md:flex md:text-2xl">
           Turning concepts into experiences.
         </div>
 
